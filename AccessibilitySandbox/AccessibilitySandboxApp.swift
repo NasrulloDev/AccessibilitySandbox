544c7fe9a9b03dd2c6dcd5ca27169977c2de2057
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-10 11:27:00 +0500

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Насрулло Исмоилжонов on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
